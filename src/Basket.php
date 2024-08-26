e6c50fffbc633ad9955264dc6b0fc63bf85207a2
Anatoly Duzenko <a.duzenko@wsmintl.com>
2024-08-26 15:08:48 -0400

    Added Basket and Delivery classes

---
+++ src/Basket.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App;
+
+use App\Widgets\BlueWidget;
+use App\Widgets\GreenWidget;
+use App\Widgets\RedWidget;
+use Exception;
+
+class Basket
+{
+    private $items = [];
+    private $discountApplied = false;
+    private $widgets = [];
+    private $specialOffers = [];
+
+    public function __construct() {
+        $this->widgets = [
+            'R01' => new RedWidget,
+            'G01' => new GreenWidget,
+            'B01' => new BlueWidget
+        ];
+        $this->specialOffers = ['R01'];
+    }
+
+    public function addProduct(string $code) :void
+    {
+        if(!in_array($code, array_keys($this->widgets))) {
+            throw new Exception("Product code does not match.");
+        }
+
+        $this->items[] = $this->widgets[$code];
+    }
+
+
+    public function totalCost() {
+        $total = 0;
+        $hasSpecialOffers = 0;
+        foreach ($this->items as $item) {
+            if(in_array($item->getCode(), $this->specialOffers)) {
+                if($hasSpecialOffers == 0 || $hasSpecialOffers > 1) {
+                    $total += $item->getPrice();
+                } else {
+                    $total += $item->getDiscountedPrice();
+                }
+                $hasSpecialOffers++;
+            } else {
+                $total += $item->getPrice();
+            }
+        }
+        
+        $delivery = new Delivery();
+        $total += $delivery->getDeliveryAmount($total);
+
+        return round($total, 2, PHP_ROUND_HALF_DOWN);
+    }
+
+    public function getItems() {
+        return $this->items;
+    }
+}
---
+++ src/Delivery.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class Delivery
+{
+    public function getDeliveryAmount(float $orderAmount) :float {
+        if ($orderAmount < 50) { 
+            return 4.95;
+        } elseif ($orderAmount >= 50 && $orderAmount < 90) {  
+            return 2.95;
+        } elseif ($orderAmount >= 90) {
+            return 0;
+        }
+    }
+}
